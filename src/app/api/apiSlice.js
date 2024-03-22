dcd4150ce2121eead3f31578ccefba0fe3ed8f4e
cs-nish <148939153+cs-nish@users.noreply.github.com>
2024-03-22 22:22:17 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://communidesk.onrender.com",
+  baseUrl: "https://communidesk-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
